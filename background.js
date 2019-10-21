2d965541de1fae22b4ee8d32e96326305cb8de83
Vineet Ramareddi <vramared@ucsc.edu>
2019-10-20 20:06:22 -0700

    ran js-beautify

--- background.js
+++ background.js
@@ -1,39 +1,42 @@
-
 chrome.alarms.clearAll();
 chrome.storage.sync.clear();
 
 var numOfWords, words, translations, notifCounter;
 
-chrome.extension.onMessage.addListener( function(request, sender, sendResponse) {
-	numOfWords = request.length;
-	words = request.sendWords;
-	translations = request.sendTransl;
-	chrome.browserAction.setPopup({popup: 'answerpop.html'});
-	notifCounter = 0;
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    numOfWords = request.length;
+    words = request.sendWords;
+    translations = request.sendTransl;
+    chrome.browserAction.setPopup({
+        popup: 'answerpop.html'
+    });
+    notifCounter = 0;
 });
 
 var question = {
-  type: "basic",
-  title:  "What does this word mean?",
-  message: "",
-  iconUrl: "images/icon.png",
+    type: "basic",
+    title: "What does this word mean?",
+    message: "",
+    iconUrl: "images/icon.png",
 };
 
 
 chrome.alarms.onAlarm.addListener(function(alarms) {
-	question.message = words[notifCounter];
+    question.message = words[notifCounter];
 
-	chrome.notifications.create(question);
-	var showWord = 'What does ' + question.message + ' mean?';
+    chrome.notifications.create(question);
+    var showWord = 'What does ' + question.message + ' mean?';
 
-	chrome.storage.sync.set({'data': showWord, 'quizAns': translations[notifCounter] }, function() {
-    	console.log('Value is set to ' + showWord);
+    chrome.storage.sync.set({
+        'data': showWord,
+        'quizAns': translations[notifCounter]
+    }, function() {
+        console.log('Value is set to ' + showWord);
     });
-	
-	notifCounter++;
-	if(notifCounter == numOfWords) {
-		chrome.alarms.clear('learnAlarm');
 
-	}
+    notifCounter++;
+    if (notifCounter == numOfWords) {
+        chrome.alarms.clear('learnAlarm');
+
+    }
 });
-
--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 document = "popup.html"
 
 var startLearningButton = document.getElementById("startLearning");
@@ -10,63 +9,63 @@ var newForm = document.createElement('form');
 var addWordsButton = document.createElement('button');
 var submitButton = document.createElement('button');
 
-if(startLearningButton) {
-  startLearningButton.addEventListener("click", createForm);
+if (startLearningButton) {
+    startLearningButton.addEventListener("click", createForm);
 }
 
 function createForm() {
-  
-  newForm.setAttribute('method', 'post');
-  formDiv.appendChild(newForm);
-  startLearningButton.parentNode.removeChild(startLearningButton);
 
-  addWordsButton.setAttribute('type', 'button');
-  addWordsButton.innerHTML = 'Add 5 translations';
-  buttonsDiv.appendChild(addWordsButton);
+    newForm.setAttribute('method', 'post');
+    formDiv.appendChild(newForm);
+    startLearningButton.parentNode.removeChild(startLearningButton);
 
-  submitButton.setAttribute('type', 'submit');
-  submitButton.innerHTML = 'Submit';
-  buttonsDiv.appendChild(submitButton);
+    addWordsButton.setAttribute('type', 'button');
+    addWordsButton.innerHTML = 'Add 5 translations';
+    buttonsDiv.appendChild(addWordsButton);
+
+    submitButton.setAttribute('type', 'submit');
+    submitButton.innerHTML = 'Submit';
+    buttonsDiv.appendChild(submitButton);
 
 }
 
-if(addWordsButton) {
+if (addWordsButton) {
 
-  addWordsButton.addEventListener("click", addTranslations);
+    addWordsButton.addEventListener("click", addTranslations);
 }
 
 var placeCounter = 0; //variable to ensure placeholders only appear in the first row of the translations
 
 function addTranslations() {
 
-  var wordCounter = 0;
-  
-  while(wordCounter < 5) {
-    var insertWord = document.createElement('input');
-    var insertTranslation = document.createElement('input');
+    var wordCounter = 0;
 
-    insertWord.setAttribute('type', 'text');
-    insertWord.setAttribute('name', 'insert');
-    
-    insertTranslation.setAttribute('type', 'text');
-    insertTranslation.setAttribute('name', 'insert');
+    while (wordCounter < 5) {
+        var insertWord = document.createElement('input');
+        var insertTranslation = document.createElement('input');
 
-    if(placeCounter == 0) {
-      insertWord.setAttribute('placeholder', 'Word');
-      insertTranslation.setAttribute('placeholder', 'Translation');
-      placeCounter++;
+        insertWord.setAttribute('type', 'text');
+        insertWord.setAttribute('name', 'insert');
+
+        insertTranslation.setAttribute('type', 'text');
+        insertTranslation.setAttribute('name', 'insert');
+
+        if (placeCounter == 0) {
+            insertWord.setAttribute('placeholder', 'Word');
+            insertTranslation.setAttribute('placeholder', 'Translation');
+            placeCounter++;
+        }
+
+        newForm.appendChild(insertWord);
+        newForm.appendChild(insertTranslation);
+        wordCounter++;
     }
 
-    newForm.appendChild(insertWord);
-    newForm.appendChild(insertTranslation);
-    wordCounter++;
-  }
-  
 }
 
-if(submitButton) {
+if (submitButton) {
 
-  submitButton.addEventListener("click", readInput);
+    submitButton.addEventListener("click", readInput);
 }
 
 var wordsArr = [];
@@ -87,55 +86,56 @@ var freq;
 
 function readInput() {
 
-  formLength = 0;
+    formLength = 0;
 
-  var i;
-  for(i = 0; i < newForm.length; i++) {
-    if(newForm.elements[i].value != '') {
-      if(((i+1) % 2) == 0){
-        translationsArr.push(newForm.elements[i].value);
-      }
-      else {
-        wordsArr.push(newForm.elements[i].value);
-      }
-      formLength++;
+    var i;
+    for (i = 0; i < newForm.length; i++) {
+        if (newForm.elements[i].value != '') {
+            if (((i + 1) % 2) == 0) {
+                translationsArr.push(newForm.elements[i].value);
+            } else {
+                wordsArr.push(newForm.elements[i].value);
+            }
+            formLength++;
+        }
+
     }
 
-  }
 
 
+    var centerDiv = document.getElementById('centerDiv');
+    if (formLength % 2 != 0) {
+        alert('Please enter a translation for each new word which you want to learn.');
+    } else if (formLength == 0) {
+        alert('Please enter at least one translation to continue.');
+    } else {
+        formLength /= 2;
+        console.log(formLength);
 
-  var centerDiv = document.getElementById('centerDiv');
-  if(formLength % 2 != 0) {
-    alert('Please enter a translation for each new word which you want to learn.');
-  }
-  else if(formLength == 0) {
-    alert('Please enter at least one translation to continue.');
-  }
-  else {
-    formLength /= 2;
-    console.log(formLength);
+        centerDiv.removeChild(document.getElementById('word-form'));
 
-    centerDiv.removeChild(document.getElementById('word-form'));
+        buttonsDiv.removeChild(addWordsButton);
+        buttonsDiv.removeChild(submitButton);
 
-    buttonsDiv.removeChild(addWordsButton);
-    buttonsDiv.removeChild(submitButton);
+        activateAlarm.setAttribute('type', 'button');
+        activateAlarm.innerHTML = 'Activate Alarm';
+        buttonsDiv.appendChild(activateAlarm);
 
-    activateAlarm.setAttribute('type', 'button');
-    activateAlarm.innerHTML = 'Activate Alarm';
-    buttonsDiv.appendChild(activateAlarm);
+        alarmDiv.appendChild(alarmMessage);
+        alarmDiv.appendChild(inputFreq);
 
-    alarmDiv.appendChild(alarmMessage);
-    alarmDiv.appendChild(inputFreq);
-
-    chrome.runtime.sendMessage({length: formLength, sendWords: wordsArr, sendTransl: translationsArr});
-  }
+        chrome.runtime.sendMessage({
+            length: formLength,
+            sendWords: wordsArr,
+            sendTransl: translationsArr
+        });
+    }
 
 }
 
 
-if(activateAlarm) {
-  activateAlarm.addEventListener("click", beginAlarm);
+if (activateAlarm) {
+    activateAlarm.addEventListener("click", beginAlarm);
 }
 
 var cancelAlarmButton = document.createElement('button');
@@ -144,27 +144,28 @@ cancelAlarmButton.innerHTML = 'Cancel Alarm';
 
 
 function beginAlarm() {
-  freq = inputFreq.value;
-  console.log(parseInt(freq));
-  if(!parseInt(freq)) {
-    alert('Please enter an integer.');
-  }
-  else if(freq < 1) {
-    alert('The alarm frequency must be at least at 1 minute intervals.');
-  }
-  else {
-    chrome.alarms.create('learnAlarm', {delayInMinutes: parseFloat(freq), periodInMinutes: parseFloat(freq)});
+    freq = inputFreq.value;
+    console.log(parseInt(freq));
+    if (!parseInt(freq)) {
+        alert('Please enter an integer.');
+    } else if (freq < 1) {
+        alert('The alarm frequency must be at least at 1 minute intervals.');
+    } else {
+        chrome.alarms.create('learnAlarm', {
+            delayInMinutes: parseFloat(freq),
+            periodInMinutes: parseFloat(freq)
+        });
 
-    buttonsDiv.removeChild(activateAlarm);
-    buttonsDiv.appendChild(cancelAlarmButton);
-  }
+        buttonsDiv.removeChild(activateAlarm);
+        buttonsDiv.appendChild(cancelAlarmButton);
+    }
 
 }
 
-if(cancelAlarmButton) {
-  cancelAlarmButton.addEventListener("click", cancelAlarm);
+if (cancelAlarmButton) {
+    cancelAlarmButton.addEventListener("click", cancelAlarm);
 }
 
 function cancelAlarm() {
-  chrome.alarms.clear('learnAlarm');
+    chrome.alarms.clear('learnAlarm');
 }
